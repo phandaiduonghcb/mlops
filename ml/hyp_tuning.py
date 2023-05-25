cd469d5b94bbdcc8049dac771d5f9060eb2511c5
phandaiduonghcb <phandaiduonghcb@gmail.com>
2023-05-25 09:45:36 +0700

    Fix small bug in hyp tuning

--- ml/hyp_tuning.py
+++ ml/hyp_tuning.py
@@ -1,7 +1,7 @@
 import sys
 import os
 if os.path.dirname(__file__) not in sys.path:
-    sys.path.insert(0, )
+    sys.path.insert(0, os.path.dirname(__file__))
 print(sys.path)
 
 from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
